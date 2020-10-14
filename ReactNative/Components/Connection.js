0d8f3586b3d6f2acd708b5325da6a58d070d0c3d
Debongnie <he201699@students.ephec.be>
2020-10-14 16:22:02 +0200

    Interactions formulaire connexion
    récuperation des valeurs et comparaison

--- ReactNative/Components/Connection.js
+++ ReactNative/Components/Connection.js
@@ -1,18 +1,39 @@
-import {Picker, StyleSheet, Text, TextInput, View, TouchableOpacity} from "react-native";
+import {StyleSheet, Text, TextInput, View, TouchableOpacity} from "react-native";
 import React from "react";
 import Inscription from "./Inscription";
 
 class Connection extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.mail = "";
+    this.password = "";
+  }
+
+  _mailChanged(name, text) {
+    this.mail = text;
+  }
+
+  _passwordChanged(name, text) {
+    this.password = text;
+  }
+
+  _checkUser(){
+    if(this.mail === "nathandebongnie@gmail.com" && this.password === "Azerty2@") {
+      console.log('connected');
+    } else {
+      console.log('User not found')
+    }
+  }
   render() {
     const nav = this.props.navigation.navigate;
     return (
       <View style={styles.component}>
         <Text style={styles.text}>E-mail : </Text>
-        <TextInput placeholder='E-mail' style={styles.input}/>
+        <TextInput placeholder='E-mail' style={styles.input} onChangeText={(text)=> this._mailChanged(this.mail, text)}/>
         <Text style={styles.text}>Mot de passe : </Text>
-        <TextInput placeholder='Mot de passe' style={styles.input}/>
-        <TouchableOpacity style={styles.button}>
+        <TextInput placeholder='Mot de passe' secureTextEntry={true} style={styles.input} onChangeText={(text)=> this._passwordChanged(this.password, text)}/>
+        <TouchableOpacity style={styles.button} onPress={()=> this._checkUser()}>
           <Text style={styles.text}>Connexion</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.connect} onPress={() => nav('Inscription')} >
--- ReactNative/Navigation/Navigation.js
+++ ReactNative/Navigation/Navigation.js
@@ -9,13 +9,15 @@ const SearchStackNavigator = createStackNavigator({
     Connection: {
         screen: Connection,
         navigationOptions: {
-            title: 'Connexion'
+            title: 'Connexion',
+            headerShown: false
         }
     },
     Inscription: { // Ici j'ai appelé la vue "Search" mais on peut mettre ce que l'on veut. C'est le nom qu'on utilisera pour appeler cette vue
         screen: Inscription,
         navigationOptions: {
-            title: 'Inscription'
+            title: 'Inscription',
+            headerShown: false
         }
     }
 });
