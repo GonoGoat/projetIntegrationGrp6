ec57c8f9aaf1d46f3363bf0aeb45c40c1104b2c6
Matthieu Luyckx <he201687@students.ephec.be>
2020-10-14 17:05:06 +0200

    Ajout static

--- Node.js/app.js
+++ Node.js/app.js
@@ -190,6 +190,209 @@ app.post('/changeDoorStatus', async (req, res) => {
 });
 
 
+/*-----------------STATIC------------------*/
+
+// LISTE DES USERS
+
+app.get('/listUsers', function (req, res) {
+    fs.readFile( __dirname + "/static/" + "users.json", 'utf8', function (err, data) {
+        console.log( data );
+        res.end( data );
+    })
+});
+
+//USER TRIER PAR ID
+
+app.get('/listUsers/:id', function (req, res) {
+    // First read existing users.
+    fs.readFile( __dirname + "/static/" + "users.json", 'utf8', function (err, data) {
+        var user = JSON.parse(data);
+        var users ;
+        for (i=0; i<user.length; i++){
+            if (user[i].id == [req.params.id]){
+                users = user[i];
+            }
+        }
+        console.log( users);
+        res.end( JSON.stringify(users));
+    })
+});
+
+//LISTE DE TAG TRIER PAR USER
+
+app.get('/listTagByUser/:id', function (req, res) {
+    // First read existing users.
+    fs.readFile( __dirname + "/static/" + "access.json", 'utf8', function (err, data) {
+        var user = JSON.parse(data);
+        var users =[];
+        for (i=0; i<user.length; i++){
+            if (user[i].users == [req.params.id]){
+                users += " - " +  user[i].tag + "\n";
+            }
+        }
+        console.log( users);
+        res.end("liste des tags pour l'utilisateur ayant l'id " + [req.params.id] + " : \n" + users);
+    })
+});
+
+//RAJOUT D UN USER
+
+app.post('/addUser/:id/:firstname/:lastname/:sexe/:mail/:password', function (req, res) {
+    // First read existing users.
+    fs.readFile( __dirname + "/static/" + "users.json", 'utf8', function (err, data) {
+        let newUser = {
+            "id":parseInt([req.params.id]),
+            "firstname":[req.params.firstname],
+            "lastname":[req.params.lastname],
+            "sexe":[req.params.sexe],
+            "mail":[req.params.mail],
+            "password":[req.params.password]
+        };
+        data = JSON.parse( data );
+        data += newUser;
+        console.log( data );
+        res.end( JSON.stringify(data));
+    })
+});
+
+// LISTE DES ACCESS PAR DOOR
+
+app.get('/listAccessByDoor/:DoorId', function (req, res) {
+    // First read existing users.
+    fs.readFile( __dirname + "/static/" + "access.json", 'utf8', function (err, data) {
+        var user = JSON.parse(data);
+        var users = [];
+        for (i=0; i<user.length; i++){
+            if (user[i].door == [req.params.DoorId]){
+                users[i] =  user[i];
+            }
+        }
+        console.log(users);
+        res.end(JSON.stringify(users));
+    })
+});
+
+//DOOR TRIER PAR ID
+
+app.get('/listDoors/:id', function (req, res) {
+    // First read existing users.
+    fs.readFile( __dirname + "/static/" + "doors.json", 'utf8', function (err, data) {
+        var door = JSON.parse(data);
+        var doors ;
+        for (i=0; i<door.length; i++){
+            if (door[i].id == [req.params.id]){
+                doors = door[i];
+            }
+        }
+        console.log( doors);
+        res.end( JSON.stringify(doors));
+    })
+});
+
+//DOORS TRIER PAR TAG
+
+app.get('/listDoorsByTag/:tag', function (req, res) {
+    // First read existing users.
+    fs.readFile( __dirname + "/static/" + "access.json", 'utf8', function (err, data) {
+        var access = JSON.parse(data);
+        var doors ;
+        for (i=0; i<access.length; i++){
+            if (access[i].tag == [req.params.tag]){
+                doors = access[i];
+            }
+        }
+        console.log( doors);
+        res.end( JSON.stringify(doors));
+    })
+});
+
+//DOOR BY TAG AND USER
+
+app.get('/listDoorsByTagAndUser/:tag/:user', function (req, res) {
+    // First read existing users.
+    fs.readFile( __dirname + "/static/" + "access.json", 'utf8', function (err, data) {
+        var access = JSON.parse(data);
+        var doors ;
+        for (i=0; i<access.length; i++){
+            if (access[i].tag == [req.params.tag] && access[i].users == [req.params.user]){
+                doors = access[i];
+            }
+        }
+        console.log(doors);
+        res.end( JSON.stringify(doors));
+    })
+});
+
+//LISTE HISTORY PAR DOOR
+
+app.get('/listHistoryByDoor/:door', function (req, res) {
+    // First read existing users.
+    fs.readFile( __dirname + "/static/" + "history.json", 'utf8', function (err, data) {
+        var history = JSON.parse(data);
+        var doors ;
+        for (i=0; i<history.length; i++){
+            if (history[i].door == [req.params.door]){
+                doors = history[i];
+            }
+        }
+        console.log(doors);
+        res.end( JSON.stringify(doors));
+    })
+});
+
+//RAJOUT D'UNE DOOR
+
+app.post('/addDoor/:id/:password/:statut', function (req, res) {
+    // First read existing users.
+    fs.readFile( __dirname + "/static/" + "doors.json", 'utf8', function (err, data) {
+        let newDoor = {
+            "id":parseInt([req.params.id]),
+            "password":[req.params.password],
+            "statut":parseInt([req.params.statut])
+        };
+        data = JSON.parse( data );
+        data += newDoor;
+        console.log( data );
+        res.end( JSON.stringify(data));
+    })
+});
+
+//RAJOUT D'UN ACCESS
+
+app.post('/addAccess/:id/:user/:tag/:nickname', function (req, res) {
+    // First read existing users.
+    fs.readFile( __dirname + "/static/" + "access.json", 'utf8', function (err, data) {
+        let newAccess = {
+            "id":parseInt([req.params.id]),
+            "user":parseInt([req.params.user]),
+            "tag":[req.params.tag],
+            "nickname":[req.params.nickname]
+        };
+        data = JSON.parse( data );
+        data += newAccess;
+        console.log( data );
+        res.end( JSON.stringify(data));
+    })
+});
+
+//RAJOUT D'UN HISTORY
+
+app.post('/addHistory/:id/:door/:users/:moment/:action', function (req, res) {
+    // First read existing users.
+    fs.readFile( __dirname + "/static/" + "history.json", 'utf8', function (err, data) {
+        let newHistory = {
+            "id":parseInt([req.params.id]),
+            "door":parseInt([req.params.door]),
+            "users":parseInt([req.params.users]),
+            "moment":[req.params.moment],
+            "action":[req.params.action]
+        };
+        data = JSON.parse( data );
+        data += newHistory;
+        console.log( data );
+        res.end( JSON.stringify(data));
+    })
+});
 
 
 
