5c113cbf47cd024ea7d1c7af7c14575c12244f33
Noscuro52 <HE201667@students.ephec.be>
2020-11-01 17:51:45 +0100

    Style listePorte

--- ReactNative/Components/listePortes.js
+++ ReactNative/Components/listePortes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, Text, TouchableHighlight, View} from 'react-native';
+import {StyleSheet, Text, TouchableHighlight, View, Dimensions} from 'react-native';
 import axios from 'axios';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {FlatList} from 'react-native-gesture-handler';
@@ -19,7 +19,7 @@ function _loadDoor (tag) {
       alert(error.message);
     })
 };
-
+const WIDTH = Dimensions.get('window').width
 class listPortes extends React.Component {
   constructor(props) {
     super(props)
@@ -30,7 +30,6 @@ class listPortes extends React.Component {
   }
   _getTag() {
     _loadTag().then(data => {
-      console.log(data);
       this.setState({
         listeTag : [ ...this.state.listeTag, ...data.data]
       })
@@ -57,51 +56,70 @@ componentDidMount() {
   render() {
   return (
     <View style={styles.MainContainer}>
-      <Text style={{ fontSize: 30, textAlign: 'center' }}>
-        Liste des portes
-      </Text>
-        <Text style={{ fontSize: 20, textAlign: 'center' }}>Cliquer sur un des tag ci-dessous pour afficher ses portes attribuées</Text>
-        <View>
-          <SafeAreaView>
-            <FlatList
+        <Text style={styles.Title}>Mes tags :</Text>
+        <View style={styles.tagContainer}>
+            <FlatList        
             data={this.state.listeTag}
             keyExtractor={(item) => item.tag}
-            renderItem={({item}) => <TouchableHighlight
-            onPress={() => this._getDoor(item)}>
-              <View style={{backgroundColor: 'white'}}>
-              <Text>{item.tag}</Text>
-              </View>
+            numColumns={3}
+            renderItem={({item}) =>
+            <TouchableHighlight
+            style={styles.tagList}  
+            onPress={() => this._getDoor(item)}
+            >
+              <Text style={styles.tagText}>{item.tag}</Text>  
             </TouchableHighlight>}
             />
-          </SafeAreaView>
-          <SafeAreaView>
+          </View>          
+          <Text style={styles.Title}>Mes portes :</Text>
+          <View style={styles.contentDoor}>
             <FlatList
             data={this.state.listeDoor}
             keyExtractor={(item) => item.door.toString()}
             renderItem={({item}) => <TouchableHighlight
             onPress={() => this._goToDetail(item)}>
-            <Text>{item.nickname}</Text>
+            <Text style={styles.doorText}>{item.nickname}</Text>
             </TouchableHighlight>}
             />
-          </SafeAreaView>
-        </View>
+            </View>      
     </View>
   )}
 };
 
 const styles = StyleSheet.create({
   MainContainer: {
-    justifyContent: 'center',
     flex: 1,
-    padding: 16,
+    paddingTop: 25
   },
-  button: {
-    alignItems: 'center',
-    backgroundColor: '#AAAAAA',
+  Title : {
+    fontSize: 15,
+    marginLeft : 20 ,
+  textDecorationLine: "underline"
+  },
+  tagContainer : {
+    padding: 20
+  },
+  tagList: {
+    flex : 1,
+    backgroundColor: 'rgb(33, 150, 243)',
     padding: 10,
-    width: '100%',
-    marginTop: 16,
+    margin : 4,
+    textAlign: "center"
   },
+  tagText : {
+    color: 'rgb(255, 255, 255)',
+  },
+  contentDoor : {
+    flex : 1,
+    borderWidth : 3,
+    margin : 30
+  },
+  doorText : {
+    borderBottomWidth : 0.5,
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    padding : 7
+  }
 });
 
 export default listPortes;
