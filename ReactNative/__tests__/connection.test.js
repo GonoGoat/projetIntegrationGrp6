5fa720072bb1d3ca1b8290167e5235c1ea4e805e
Debongnie <he201699@students.ephec.be>
2020-11-07 10:32:37 +0100

    Tests Connexion
    Débuts avec Jest

--- ReactNative/__tests__/connection.test.js
+++ ReactNative/__tests__/connection.test.js
@@ -1,6 +1,12 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
 import Connection from "../Components/Connection";
 
 test('update props mail', () => {
+  const myTest = Connection.test('string')
+  expect(test).toHaveBeenCalled();
+  const tree = renderer.create(<Connection />).toJSON();
   let string = "mail_example";
-  expectToBeCalledOnce(Connection._mailChanged())
+  tree._setMail(string);
+  expect(Connection.mail).toBe("mail_example");
 });
