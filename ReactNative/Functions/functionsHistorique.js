f3585ba134060f591d77be2c5cbff0608beb211d
Matthieu Luyckx <he201687@students.ephec.be>
2020-11-17 19:47:25 +0100

    Réagencement pour tests + Tests unitaires historique

---
+++ ReactNative/Functions/functionsHistorique.js
@@ -0,0 +1,82 @@
+export function getDateFormat(date) {
+  var dateFormatee = "";
+  dateFormatee += date[8];
+  dateFormatee += date[9];
+  dateFormatee += "/";
+  dateFormatee += date[5];
+  dateFormatee += date[6];
+  dateFormatee += "/";
+  dateFormatee += date[0];
+  dateFormatee += date[1];
+  dateFormatee += date[2];
+  dateFormatee += date[3];
+  dateFormatee += " ";
+  dateFormatee += date[11];
+  dateFormatee += date[12];
+  dateFormatee += ":";
+  dateFormatee += date[14];
+  dateFormatee += date[15];
+  return dateFormatee;
+}
+
+export function getNomPrenom(id, table) {
+  for(var i=0; i<table.length; i++) {
+    if(table[i].id == id) {
+      var nomPrenom = table[i].lastname + " " + table[i].firstname;
+      return nomPrenom;
+    }
+  }
+}
+
+export function getStyleByIntex(index) {
+  let itemHistoPair =  {
+    backgroundColor: '#719ada',
+    marginLeft: 15,
+    marginRight: 15,
+    paddingLeft: 15,
+    paddingVertical: 10
+  }
+  let itemHistoImpair = {
+    backgroundColor: '#d0d0d0',
+    marginLeft: 15,
+    marginRight: 15,
+    paddingLeft: 15,
+    paddingVertical: 10
+  }
+
+  if(index%2 == 0) { 
+    return itemHistoPair
+  } else {
+    return itemHistoImpair
+  }
+}
+
+export function getActionStyle(index) {
+  let actionImpair = {
+    position: "absolute",
+    color: "black",
+    top: 15,
+    right: 10,
+    fontSize: 20
+  }
+  let actionPair = {
+    position: "absolute",
+    color: "white",
+    top: 15,
+    right: 10,
+    fontSize: 20
+  }
+  if(index%2 == 0) {
+    return actionPair
+  } else {
+    return actionImpair
+  }
+}
+
+export function getActionString(action) {
+  if(action == 1) {
+    return "Ouverture"
+  } else {
+    return "Fermeture"
+  }
+}
--- ReactNative/__tests__/Historique.test.js
+++ ReactNative/__tests__/Historique.test.js
@@ -1,89 +1,77 @@
-import React from "react"
-import Enzyme,{shallow, mount, render} from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import Historique from '../Components/Historique'
+import 'react-native';
+import React from 'react';
+import { getDateFormat, getNomPrenom, getStyleByIntex, getActionStyle, getActionString } from '../Functions/functionsHistorique'
+import { state } from "../Components/Historique";
 
-Enzyme.configure({ adapter: new Adapter() });
-let param = {params: {doorIdParam: 1, nickname: "Testing"}}
+it('Send correct date format', () => {
+  let sendedDate = '2000-01-01T00:00:00.000Z';
+  let receivedDate = getDateFormat(sendedDate)
+  expect(receivedDate).toBe('01/01/2000 00:00')
+})
 
-beforeEach(() => {
-  jest.useFakeTimers();
-});
+it('Returns correct name format', () => {
+  let id = 1;
+  let table = [{"id":1,"firstname":"testingFirstname","lastname":"testingLastname","phone":null,"sexe":"M","mail":"testing@testing.testing","password":"testing"}]
+  let receivedName = getNomPrenom(id, table)
+  expect(receivedName).toBe('testingLastname testingFirstname')
+})
 
-describe('Historique', () => {
-  it('From 1 to Ouverture', () => {
-    const wrapper = shallow(<Historique route={param}/>);
-    const componentInstance = wrapper.instance();
-    expect(componentInstance.getActionString(1)).toBe("Ouverture");
+it('Returns correct style by pair Index', () => {
+  let index = 0
+  let receivedStyle = getStyleByIntex(index)
+  expect(receivedStyle).toMatchObject({
+    backgroundColor: '#719ada',
+    marginLeft: 15,
+    marginRight: 15,
+    paddingLeft: 15,
+    paddingVertical: 10
   })
-  it('From 0 to Fermeture', () => {
-    const wrapper = shallow(<Historique route={param}/>);
-    const componentInstance = wrapper.instance();
-    expect(componentInstance.getActionString(0)).toBe("Fermeture");
+})
+
+it('Returns correct style by unpair Index', () => {
+  let index = 1
+  let receivedStyle = getStyleByIntex(index)
+  expect(receivedStyle).toMatchObject({
+    backgroundColor: '#d0d0d0',
+    marginLeft: 15,
+    marginRight: 15,
+    paddingLeft: 15,
+    paddingVertical: 10
   })
-  it('Send correct date format', async () => {
-    const wrapper = shallow(<Historique route={param}/>);
-    const componentInstance = wrapper.instance();
-    expect(componentInstance.getDateFormat('2020-11-12T09:05:26.586Z')).toBe('12/11/2020 09:05');
-  });
-  it('Gives correct name', async () => {
-    const wrapper = shallow(<Historique route={param}/>);
-    wrapper.setState({users:[{"id":1,"firstname":"testingFirstname","lastname":"testingLastname","phone":"testing","sexe":"testing","mail":"testing.testing@testing.testing","password":"testing"}]}); 
-    const componentInstance = wrapper.instance();
-    expect(componentInstance.getNomPrenom(1)).toBe('testingLastname testingFirstname');
-  });
-  it('Send correct index style pair', async () => {
-    const wrapper = shallow(<Historique route={param}/>);
-    const componentInstance = wrapper.instance();
-    expect(componentInstance.getStyleByIntex(0)).toMatchObject({
-      backgroundColor: '#719ada',
-      marginLeft: 15,
-      marginRight: 15,
-      paddingLeft: 15,
-      paddingVertical: 10});
-  });
-  it('Send correct index style unpair', async () => {
-    const wrapper = shallow(<Historique route={param}/>);
-    const componentInstance = wrapper.instance();
-    expect(componentInstance.getStyleByIntex(1)).toMatchObject({
-      backgroundColor: '#d0d0d0',
-      marginLeft: 15,
-      marginRight: 15,
-      paddingLeft: 15,
-      paddingVertical: 10});
-  });
-  it('Send correct action style pair', async () => {
-    const wrapper = shallow(<Historique route={param}/>);
-    const componentInstance = wrapper.instance();
-    expect(componentInstance.getActionStyle(0)).toMatchObject({
-      position: "absolute",
-      color: "white",
-      top: 15,
-      right: 10,
-      fontSize: 20
-    });
-  });
-  it('Send correct action style unpair', async () => {
-    const wrapper = shallow(<Historique route={param}/>);
-    const componentInstance = wrapper.instance();
-    expect(componentInstance.getActionStyle(1)).toMatchObject({
-      position: "absolute",
-      color: "black",
-      top: 15,
-      right: 10,
-      fontSize: 20
-    });
+})
+
+it('Returns correct style for pair action', () => {
+  let index = 0
+  let receivedStyle = getActionStyle(index)
+  expect(receivedStyle).toMatchObject({
+    position: "absolute",
+    color: "white",
+    top: 15,
+    right: 10,
+    fontSize: 20
   })
-  it('Renders when loaded and histo empty', async () => {
-    const wrapper = shallow(<Historique route={param}/>);
-    const componentInstance = wrapper.instance();
-    wrapper.setState({isLoading: false});
-    expect(componentInstance.render()).not.toBe('<Text>Loading...</Text>');
+})
+
+it('Returns correct style for unpair action', () => {
+  let index = 1
+  let receivedStyle = getActionStyle(index)
+  expect(receivedStyle).toMatchObject({
+    position: "absolute",
+    color: "black",
+    top: 15,
+    right: 10,
+    fontSize: 20
   })
-  it('Renders when loaded and histo not empty', async () => {
-    const wrapper = shallow(<Historique route={param}/>);
-    const componentInstance = wrapper.instance();
-    wrapper.setState({isLoading: false, histo: [{"id":1,"door":1,"users":1,"date":"2000-01-01T00:00:00.693Z","action":1}]});
-    expect(componentInstance.render()).not.toBe('<Text>Loading...</Text>');
-  })
-});
+})
+
+it('Returns -Fermeture- for pair action', () => {
+  let action = 0
+  let receivedStyle = getActionString(action)
+  expect(receivedStyle).toBe('Fermeture')
+})
+
+it('Returns -Ouverture- for unpair action', () => {
+  let action = 1
+  let receivedStyle = getActionString(action)
+  expect(receivedStyle).toBe('Ouverture')
+})
