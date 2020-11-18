618dfbbc54eed3e0f77c4a340a70c8ef835d40e6
Matthieu Luyckx <he201687@students.ephec.be>
2020-11-18 21:27:09 +0100

    Update confirmation de suppression

--- ReactNative/Components/PorteDetail.js
+++ ReactNative/Components/PorteDetail.js
@@ -1,9 +1,12 @@
 
 import React, { Component } from 'react';
-import {Alert , Button, StyleSheet, Text, View, TextInput, TouchableOpacity,TouchableHighlight} from 'react-native';
+import {StyleSheet, Text, View,TouchableHighlight} from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
+import {Modal} from "react-native-paper";
 import axios from 'axios';
 import { getStatus, getDoorById, getTitle } from '../Functions/functionsPorteDetail'
+
+
 export default class PorteDetail extends React.Component {
 
   constructor(props){
@@ -77,29 +80,8 @@ export default class PorteDetail extends React.Component {
       });
   }
 
-  confirmDelete(userId, doorId) {
-    Alert.alert(  
-      'Suppression',  
-      'Voulez-vous vraiment supprimer cette porte de votre liste ?',  
-      [  
-          {  
-              text: 'Annuler',  
-                  
-          },  
-          {
-              text: 'OK',
-              onPress: () => (this.deleteAccess(userId, doorId),
-                Alert.alert(  
-                  'Suppression',  
-                  'Porte supprimée'
-                )
-              )
-          }  
-      ]  
-  );
-  }
-
   deleteAccess(userId, doorId) {
+    this.setState({modalVisible: false})
     const params = {
       door: doorId,
       users : userId,
@@ -107,6 +89,7 @@ export default class PorteDetail extends React.Component {
     axios.post('http://192.168.0.29:8081/access/delete',{params})
       .then(res => {
         this.props.navigation.push("Accueil")
+        this.setState({isLoading: false})
       })
       .catch(err => {
           console.log(err),
@@ -132,8 +115,6 @@ export default class PorteDetail extends React.Component {
       const doorIdParam = this.props.route.params.doorIdParam;
       const nickname = this.props.route.params.nickname;
       const tagName = this.props.route.params.tagName;
-
-
       const nav = this.props.navigation.navigate;
       
       var dataDoor =  getDoorById(doorIdParam, this.state.doors);
@@ -146,8 +127,8 @@ export default class PorteDetail extends React.Component {
               <Icon.Button  
               name="ios-trash" 
               size={30} 
-              onPress={() => this.confirmDelete(8,doorIdParam)}
-              style={{backgroundColor: "#719ada",}} >
+              onPress={() => this.setState({modalVisible: true})}
+              style={{backgroundColor: "#719ada",}}>
                 Delete door
               </Icon.Button>
             </View>
@@ -198,7 +179,25 @@ export default class PorteDetail extends React.Component {
               </View>
             </TouchableHighlight>
           </View>
+          <Modal visible={this.state.modalVisible} contentContainerStyle={{backgroundColor: 'white', padding: 20}}>
+            <Text style={{fontSize: 18, textAlign: "center", marginBottom: 5}}>Voulez-vous vraiment supprimer cette porte ?</Text>
+            <Text style={{fontSize: 11, textAlign: "center", color:"red"}}>Attention !</Text>
+            <Text style={{fontSize: 8, textAlign: "center", marginBottom: 60}}>Cette action est irréversible. Pour trouver à nouveau cette porte dans vos portes enregistrées, vous aurez à nouveau besoin de l'identifiant et du mot de passe de cette porte.</Text>
+            <TouchableHighlight style={styles.cancelModal}
+              onPress={() => this.setState({modalVisible: false})}>
+              <View>
+                <Text style={{fontSize: 20}}>Non</Text>
+              </View>
+            </TouchableHighlight>
+            <TouchableHighlight style={styles.okModal}
+              onPress={() => this.deleteAccess(8, doorIdParam)}>
+              <View>
+                <Text style={{fontSize: 20}}>Oui</Text>
+              </View>
+            </TouchableHighlight>
+          </Modal>
         </View>
+        
       );
     }
   }
@@ -339,5 +338,42 @@ const styles = StyleSheet.create({
   modalText: {
     marginBottom: 15,
     textAlign: "center"
-  }
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+},
+modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+        width: 0,
+        height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5
+},
+cancelModal: {
+  position: "absolute",
+  bottom: 10,
+  left: 100,
+  backgroundColor: "#d0d0d0",
+  paddingHorizontal: 25,
+  paddingVertical: 10
+},
+okModal: {
+ position: "absolute",
+ bottom: 10,
+ right: 100,
+ backgroundColor: '#719ada',
+ paddingHorizontal: 25,
+  paddingVertical: 10
+}
 })
