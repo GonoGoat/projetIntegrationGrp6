26cf661baff122ead21080366b85389d3d7ae5f1
GonoGoat <he201654@students.ephec.be>
2020-11-21 20:30:58 +0100

    Update test utilitaire

--- ReactNative/Functions/functionsAjoutPorte.js
+++ ReactNative/Functions/functionsAjoutPorte.js
@@ -170,4 +170,3 @@ export function checkAjoutAPI(req,type) {
         }
     }
 }
-    
--- ReactNative/__tests__/utils.ajoutPorte.test.js
+++ ReactNative/__tests__/utils.ajoutPorte.test.js
@@ -1,4 +1,5 @@
-const fcts = require("../utils/ajoutPorte")
+//npm test -- utils.ajoutPorte.test.js
+const fcts = require("../Functions/functionsAjoutPorte")
 
 describe ("checkVerif function" , () => {
     it('fails with at least one empty fields', () => {
@@ -106,6 +107,133 @@ describe ("checkAjout function" , () => {
 
         test.push(fcts.checkAjout('HELLOworld','testeTestE'));
         test.push(fcts.checkAjout('testeTestE','HELLOworld'));
+
+        for (let i = 0; i<test.length;i++) {
+            expect(test[i].hasOwnProperty("type")).toBeFalsy();
+            expect(test[i] === true).toBeTruthy();
+        }
+    });
+});
+
+describe ("checkVerifAPI function" , () => {
+    it('fails if the type is set at "true" but the data isn\'t correct', () => {
+        let isExisting = {
+            data : false
+        };
+        let badAnswer = {
+            data : 0
+        };
+        let test;
+        let invalidText = "Vous avez déjà cette porte";
+        let problemText = "Votre requête a abouti";
+
+        test = fcts.checkVerifAPI(isExisting,true);
+        expect(test.hasOwnProperty("type")).toBeTruthy();
+        expect(test.type === "fail").toBeTruthy();
+        expect(test.message).toContain(invalidText);
+        expect(test.message).not.toContain(problemText);
+
+        test = fcts.checkVerifAPI(badAnswer,true);
+        expect(test.hasOwnProperty("type")).toBeTruthy();
+        expect(test.type === "fail").toBeTruthy();
+        expect(test.message).toContain(problemText);
+        expect(test.message).not.toContain(invalidText);
+
+    })
+    it('fails if the type is set at "false"', () => {
+        let badPassword = {
+            response : {
+                status : 403
+            }
+        };
+        let invalidId = {
+            response : {
+                status : 404
+            }
+        };
+        let serverBroken = {
+            response : {
+                status : 500
+            }
+        };
+        let badGateway = {
+            response : {
+                status : 502
+            }
+        };
+        let badRequest = {
+            request : {
+                test : "test"
+            }
+        }
+        let error = {
+            test : "test"
+        }
+        let idText = "ID"
+        let mdpText = "Mot de passe"
+        let erreur = "erreur"
+        let req = "envoi"
+
+        test = fcts.checkVerifAPI(badPassword,false);
+        expect(test.hasOwnProperty("type")).toBeTruthy();
+        expect(test.type === "fail").toBeTruthy();
+        expect(test.message).toContain(mdpText);
+        expect(test.message).not.toContain(idText);
+        expect(test.message).not.toContain(erreur);
+
+        test = fcts.checkVerifAPI(invalidId,false);
+        expect(test.hasOwnProperty("type")).toBeTruthy();
+        expect(test.type === "fail").toBeTruthy();
+        expect(test.message).toContain(idText);
+        expect(test.message).not.toContain(mdpText);
+        expect(test.message).not.toContain(erreur);
+
+        test = fcts.checkVerifAPI(serverBroken,false);
+        let test2 = fcts.checkVerifAPI(badGateway,false);
+
+        expect(test.hasOwnProperty("type")).toBeTruthy();
+        expect(test.type === "fail").toBeTruthy();
+        expect(test.message).toContain(erreur);
+        expect(test.message).not.toContain(mdpText);
+        expect(test.message).not.toContain(idText);
+
+        expect(test2.hasOwnProperty("type")).toBeTruthy();
+        expect(test2.type === "fail").toBeTruthy();
+        expect(test2.message).toContain(erreur);
+        expect(test2.message).not.toContain(mdpText);
+        expect(test2.message).not.toContain(idText);
+
+        expect(test.message).toEqual(test2.message);
+
+        test = fcts.checkVerifAPI(badRequest,false);
+        expect(test.hasOwnProperty("type")).toBeTruthy();
+        expect(test.type === "fail").toBeTruthy();
+        expect(test.message).toContain(req);
+        expect(test.message).toContain(erreur);
+        expect(test.message).not.toContain(idText);
+        expect(test.message).not.toContain(mdpText);
+
+        test = fcts.checkVerifAPI(error,false);
+        expect(test.hasOwnProperty("type")).toBeTruthy();
+        expect(test.type === "fail").toBeTruthy();
+        expect(test.message).toContain(erreur);
+        expect(test.message).not.toContain(idText);
+        expect(test.message).not.toContain(mdpText);
+
+        expect(test.message).toEqual(test2.message);
+    })
+
+    it('works if the type is set at "true" and the data is correct', () => {
+        let test = [];
+        let good ={
+            data : true,
+        };
+        let alsoGood = {
+            data : true,
+            otherData : true
+        }
+        test.push(fcts.checkVerifAPI(good,true));
+        test.push(fcts.checkVerifAPI(alsoGood,true))
 
         for (let i = 0; i<test.length;i++) {
             expect(test[i].hasOwnProperty("type")).toBeFalsy();
