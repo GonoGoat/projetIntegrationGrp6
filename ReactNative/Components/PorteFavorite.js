94191f3c65f6833b8d86095daa5009371cef0f6b
Noscuro52 <HE201667@students.ephec.be>
2020-12-02 16:09:25 +0100

    Porte Favorite error handler + style

--- ReactNative/Components/PorteFavorite.js
+++ ReactNative/Components/PorteFavorite.js
@@ -13,8 +13,9 @@ class PorteFavorite extends React.Component {
     super(props)
     this.state = {
       listePorte: [],
-      switchValue : false,
-      reload: true   
+      erreur : false,
+      reload: true,
+      backgroundColor : ""
     }
   }
   loadDoors() {
@@ -22,7 +23,11 @@ class PorteFavorite extends React.Component {
     this.setState({
       listePorte : []
     })
-    for(let i = 0; i < 6; i=i+2) {
+    if((idPorte === "undefined")) {
+      this.setState({erreur: true})
+  }
+  else if(idPorte[0].length > 0) {
+        for(let i = 0; i < idPorte[0].length +1; i=i+2) {
        axios
         .get('http://localhost:8081/doorIdUser/' + idPorte[0][i] + "/" + user)
         .catch(function(error) {
@@ -35,21 +40,27 @@ class PorteFavorite extends React.Component {
           }
         })
         .then(response => {
+          if(response) {
           this.setState({
             listePorte : [ ...this.state.listePorte, ...response.data]
-          })          
+          })
+        }   
+        else {
+          this.setState({erreur: true})
+        }       
         })
     }
+  }
   }
 
  loadIdDoor() {
   AsyncStorage.getItem('doors', function(errs, result) {
     if (!errs) {
-      if (result !== null) {
+      if (result.length > 0) {
         idPorte.push(result)
       }
       else {
-
+        idPorte = "undefined"
       }
     }
   })
@@ -59,7 +70,7 @@ class PorteFavorite extends React.Component {
         user = result
       }
       else {
-
+        console.log("pas connecté")
       }
     }
   })
@@ -125,27 +136,37 @@ sendHistory(doorId, status) {
         console.log(err)
     });
 }
+ getColorButton(boolStatus) {
+  if(boolStatus == true) {
+      return "#DC143C";
+  }
+  else {
+      return "limegreen";
+  }
+}
 
 renderItem = ({item}) => {
   var statusString = getStatus(item.status);
+  var colorButton = this.getColorButton(item.status)
+  var colorText = this.getColorButton(!item.status)
   return (
     <View style={styles.zonePorte}>
-  <Text>Nom : {item.nickname}</Text>
-  <Text>Tag : {item.tag}</Text>
-  <Text style={{position: "absolute", right: 30, fontSize: 20}}>Status : {statusString}</Text>
-  <TouchableHighlight style={styles.openButton}
+  <Text style={styles.textPorte}>Nom : {item.nickname}</Text>
+  <Text style={styles.textPorte}>Tag : {item.tag}</Text>
+  <Text style={{position: "absolute" , right: 10, fontSize: 20}}>Status : <Text style={{color: colorText, fontSize: 20}}>{statusString}</Text></Text>
+  <TouchableHighlight style={{backgroundColor : colorButton, marginTop : 10}}
               onPress={() => this.openDoor(item.id, item.status)}>
               <View>
-                <Text style={{fontSize: 20}}>{getTitle(item.status)}</Text>
+                <Text style={{fontSize: 20, textAlign : "center"}}>{getTitle(item.status)}</Text>
               </View>
   </TouchableHighlight>
   </View>
   )
 }
   render() {
+    if (this.state.erreur === false) {
     return (
       <View style={styles.container}>
-        <Text style={styles.Text}>Accueil</Text>
         <FlatList        
             data={this.state.listePorte}
             keyExtractor={(item) => item.id.toString()}
@@ -154,6 +175,31 @@ renderItem = ({item}) => {
             />
       </View>
     )}
+    else if((this.state.erreur === true) && (idPorte == "undefined") && (user > 0)) {
+      return (
+        <View>
+          <View style={styles.MainContainer}>
+            <View style={styles.tagContainer}>
+                <Text style={styles.Title}>Vous ne possédez pas de porte favorite pour l'instant.</Text>
+                <TouchableHighlight style={styles.addDoor} onPress={() => this.props.navigation.navigate('Ajouter une porte')}>
+                  <Text style={styles.tagText}  >Commencez par ajouter une porte</Text>
+                  </TouchableHighlight>
+              </View>          
+                </View>      
+        </View>
+      )
+    }
+    else {
+      return(
+<View>
+      <Text style={styles.erreur}>Une erreur est survenu</Text>
+      <TouchableHighlight style={styles.addDoor} onPress={() => this.props.navigation.navigate('Connexion')}>
+              <Text style={styles.tagText}  >Rendez-vous à la page de connexion</Text>
+              </TouchableHighlight>
+    </View>
+    )
+    }
+  }
 }
 
 export default PorteFavorite;
@@ -168,14 +214,39 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center'
   },
+  Title : {
+    fontSize: 15,
+    marginLeft : 20,
+    textAlign: "center"
+  },
   zonePorte : {
     flex : 1,
-    padding : 45,
+    padding : 30,
     borderWidth : 3,
-    margin : 20
+    margin : 15
   },
   switch : {
     flex : 3,
     marginLeft : 50
+  },
+  erreur : {
+    fontSize: 30,
+    padding: 20,
+    textAlign:"center"
+  },
+  addDoor : {
+    flex : 1,
+    backgroundColor: 'rgb(33, 150, 243)',
+    padding: 10,
+    margin : 4,
+    textAlign: "center"
+  },
+  tagText : {
+    color: 'rgb(255, 255, 255)',
+  },
+  textPorte : {
+    fontSize : 15,
+    fontWeight : "bold",
+    left: 0
   }
 });
