8bfad1f9306bbdfe23bf21335660b2f3cf04cf9b
Noscuro52 <HE201667@students.ephec.be>
2020-12-04 16:37:59 +0100

    Tests Us 13
    Test basique sur l'appel API de l'US13

---
+++ ReactNative/__tests__/PorteFavorite.test.js
@@ -0,0 +1,24 @@
+import {loadDoors} from '../Functions/functionPorteFavorite'
+
+const axios = require('axios');
+jest.mock('axios');
+
+describe('getDoors', () => {
+    it('return correct Doors information by user', async () => {
+       axios.get.mockResolvedValue({
+         data: [{"id":1,"nickname":"Casa","tag":"LLN","status":1,"adresseip":"192.168.1.60"}]
+       });
+       const Doors = await loadDoors();
+       expect(Doors).toEqual("Casa");
+     });
+   
+   //test .catch
+     it('detect when an error occured', async () => {
+       const errorMessage = 'Network Error';
+       axios.get.mockImplementationOnce(() =>
+         Promise.reject(new Error(errorMessage)),
+       );
+       const tagError = await loadDoors(33)
+       expect(tagError).toEqual(errorMessage)
+     })
+   })
