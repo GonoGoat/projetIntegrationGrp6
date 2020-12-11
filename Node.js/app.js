1f3f33192c95d172217153781e9d5af7583e6318
Noscuro52 <HE201667@students.ephec.be>
2020-12-11 17:44:37 +0100

    Correction bug

--- Node.js/app.js
+++ Node.js/app.js
@@ -337,6 +337,20 @@ app.get('/doorTagUser/:tag/:users', async (req, res) => {
 });
 
 /*************************************************
+		GET DOOR BY SPECIFIC DOOR ID & USER
+*************************************************/	//TEST OK
+
+app.get('/doorIdUser/:door/:users', async (req, res) => {
+    let door=req.params.door;
+    let users=req.params.users;
+    let sql = 'select door.id,access.nickname,access.tag,door.status,door.adresseip from access inner join door on access.door =  door.id where door.id = ' +  door + 'and access.users =  \'' + users + '\'';
+    pool.query(sql, (err, rows) => {
+      if (err) throw err;
+      return res.send(rows.rows);
+    })
+  });
+
+/*************************************************
 		GET TAGS BY USER
 *************************************************/	//TEST OK
 
