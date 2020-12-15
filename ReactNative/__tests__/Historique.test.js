ea06623cf3ec706f67ea5fdccf28f73bfbc63eae
Matthieu Luyckx <he201687@students.ephec.be>
2020-12-15 23:10:50 +0100

    Correction tests

---
+++ ReactNative/__tests__/Historique.test.js
@@ -0,0 +1,42 @@
+import React from "react"
+import Enzyme,{shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import Historique from '../Components/Historique';
+
+import axios from 'axios';
+import { FlatList } from "react-native";
+
+jest.mock('axios');
+
+Enzyme.configure({ adapter: new Adapter() });
+/*
+describe('Historique', () => {
+    it('should render without crashing when histo not empty', () => {
+        axios.get.mockResolvedValue({
+            data : [{"id":1,"door":1,"users":1,"date":"2020-12-11T17:21:47.240Z","action":0}]
+        })
+        let route = {params: {doorIdParam: 1, nickname:"Testing"}}
+        wrap = shallow(<Historique route={route}/>)
+        wrap.setState({isLoading: false, histo: ""})
+        expect(wrap.exists()).toBe(true);
+    })
+
+    it('should render without crashing when histo empty', () => {
+        let route = {params: {doorIdParam: 1, nickname:"Testing"}}
+        wrap = shallow(<Historique route={route}/>)
+        wrap.setState({isLoading: false, histo: ""})
+        expect(wrap.exists()).toBe(true);
+    })
+
+    it('should error at axios.get', () => {
+        axios.get.mockRejectedValue({
+            response : {
+                status : 403
+            }
+        })
+        let route = {params: {doorIdParam: 1, nickname:"Testing"}}
+        wrap = shallow(<Historique route={route}/>)
+        setTimeout(function(){ expect(wrap.state().errorVisible).toEqual(true);}, 100);
+    })
+})*/
